bc1e10135d685409bf69e99c28a27e21a2a60d98
vishalned <vishalned@gmail.com>
2017-08-29 21:22:16 +0530

    added UI

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # web-game_mindpalace
 website based game
-
-# Ranking Algorithm
-(((((currlivezone)*(10))/(questionsattempted))*(currlivezone)) - (NoTrialVisits))
--- php/clearRankList.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $name = "../data/Ranking.csv";
-    $file = fopen($name,"w") or exit("error opening file");
-    fclose($file);
-    echo "cleared"
- ?>
