4434962be513a9964e66e14770945eaa7968f57e
saitejach1998 <saiteja.ch1998@gmail.com>
2017-09-11 10:49:08 +0530

    ranking updated

--- php/ranking.php
+++ php/ranking.php
@@ -11,26 +11,51 @@
     $teamNameList = array();
     $scoreList = array();
 
-    for($i = 0;$i<$size;$i++) {
+    if( $size == 0) {
+    	exit("No names in ranklist error.");
+    }
+    for($i = 0;$i<$size-1;$i++) {
         $parts = explode(",",$rankfile[$i]);
-        $teamNameList[$i] = $parts[1];
-        $scoreList[$i] = (int)substr($parts[2],0,sizeof($parts[2])-2);
+        if($teamName <> $parts[1]){
+		$teamNameList[$i] = $parts[1];
+		$scoreList[$i] = (int)substr($parts[2],0,sizeof($parts[2])-2);
+	}
     }
 
     $rankfile = fopen($file,"w");
 
-    $scoreList[array_search($teamName,$teamNameList)] = $score;
-    array_multisort($scoreList, SORT_NUMERIC, SORT_DESC, $teamNameList);
-
-    for ($j=0; $j <$size ; $j++) {
-        $data = ($j+1).",".$teamNameList[$j].",".$scoreList[$j]."\n";
+    if($size == 1){
+        $data = ($j+1).",".$teamName.",".$score."\n";
         fwrite($rankfile,$data);
     }
-    echo var_dump($scoreList);
+    else{
+        $save = $size-1;
+        for ($j=0; $j <$size-1 ; $j++) {
+
+            if($score > $scoreList[$j]){
+                $save = $j;
+            	break;
+            }
+            else{
+                $data = ($j+1).",".$teamNameList[$j].",".$scoreList[$j]."\n";
+                fwrite($rankfile,$data);
+            }
+        }
+
+        $data = ($save+1).",".$teamName.",".$score."\n";
+        fwrite($rankfile,$data);
+
+        for ($k=$save; $k <$size-1 ; $k++) {
+
+            $data = ($k+2).",".$teamNameList[$k].",".$scoreList[$k]."\n";
+            fwrite($rankfile,$data);
+        }
+    }
+    //echo var_dump($scoreList);
     //echo var_dump($scoreList);
     fclose($rankfile);
 
-    //echo var_dump($teamNameList);
+    echo var_dump($teamNameList);
     //echo $score
     //echo var_dump($scoreList);
  ?>
